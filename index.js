8ca6575c2f7b4bfdae73e1b9de051e17606bb525
Newtonstein <82240046+Newtonstein@users.noreply.github.com>
2021-08-24 14:49:02 -0400

    images fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "Images/" + randomDiceImage
+var randomImageSource = "images/" + randomDiceImage
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -13,7 +13,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "Images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
